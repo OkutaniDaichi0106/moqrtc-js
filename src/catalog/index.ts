158b89e1e0aab8d9539afc4a0c0404b205ff87f4
daichiDeskTop <daichi1616@outlook.jp>
2025-10-26 02:01:03 +0900

    Move source files from gomoqt/hang-web

---
+++ src/catalog/index.ts
@@ -0,0 +1,7 @@
+export * from "./container"
+export * from "./init"
+export * from "./track"
+export * from "./descriptors"
+
+export const CATALOG_TRACK_NAME = "catalog.json";
+
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-/**
- * moqrtc-ts - TypeScript implementation of Media over QUIC (MOQ) protocol
- *
- * This is the main entry point for the library.
- * Implementation will be added in future development.
- */
-
-// Export placeholder - implementation to be added
-export {};
+export * from "./broadcast";
+export * from "./room";
+export * from "./member";
+export * from "./elements";
